8faf22695b9d2ccb25238ccc918cb38a76d2dbd4
sjkim04 <38371178+sjkim04@users.noreply.github.com>
2021-09-29 10:20:19 +0900

    Update code.js
    Add function that if question is not answered, the code blockes the rest of the program

--- code.js
+++ code.js
@@ -1,13 +1,16 @@
-var time = prompt('解答を見る秒数をご記入ください。(max 180)\n解答開閉ボット by のむチュウ', '');
-if(isNaN(time)) {
-  alert('入力されたものが数字ではありません');
-} else if(Number(time)>180) {
-  alert('数字が大きすぎます'); }
-else if(time == '') {
-  alert('何も入力されていません');
-} else {
-  drill.openExplainDialog(true);
-  setTimeout(function() {
-    drill.closeExplainDialog(true);
-  },time*1000);
-};
+if ($('#panel-judge').attr('button_mode') == 'judge') {
+  alert('回答が入力されていません');
+else {
+  var time = prompt('解答を見る秒数をご記入ください。(max 180)\n解答開閉ボット by のむチュウ', '');
+  if(isNaN(time)) {
+    alert('入力されたものが数字ではありません');
+  } else if(Number(time)>180) {
+    alert('数字が大きすぎます');
+  } else if(time == '') {
+    alert('何も入力されていません');
+  } else {
+    drill.openExplainDialog(true);
+    setTimeout(function() {
+      drill.closeExplainDialog(true);
+    },time*1000)
+}
